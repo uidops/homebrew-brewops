eeb82c60adc25d3edb507c2922dcf06e7129db7d
uidops <uidops@protonmail.com>
2025-11-28 19:04:29 +0330

    add bicon formula

---
+++ Formula/bicon.rb
@@ -0,0 +1,37 @@
+class Bicon < Formula
+  desc "Bicon for macOS — bidirectional console support"
+  homepage "https://github.com/uidops/bicon-macos"
+  url "https://github.com/uidops/bicon-macos/archive/refs/heads/main.tar.gz"
+  version "HEAD"
+  sha256 "SKIP" # Homebrew allows sha256 :no_check for HEAD-like formulas
+  license "GPL-2.0-or-later"
+
+  # Dependencies required to run autogen.sh + build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool"  => :build
+  depends_on "pkg-config" => :build
+
+  # Libraries used by bicon
+  depends_on "freetype"
+  depends_on "gettext"
+
+  # HEAD version for convenience
+  head "https://github.com/uidops/bicon-macos.git", branch: "main"
+
+  def install
+    # In case glibtoolize is needed instead of libtoolize
+    ENV["LIBTOOLIZE"] = "glibtoolize"
+
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    # Simple version check if binary exists
+    assert_match "bicon", shell_output("#{bin}/bicon --help 2>&1")
+  end
+end
