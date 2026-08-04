43b29e57645dd90a8be8be6d3afaff0dd1c9655b
Javad <uidops@protonmail.com>
2026-08-04 12:58:41 +0330

    Update Ganjoor cask to 1.0.4

--- Casks/ganjoor.rb
+++ Casks/ganjoor.rb
@@ -1,6 +1,6 @@
 cask "ganjoor" do
-  version "1.0.3"
-  sha256 "901448df1fc30c6167a49a7dbde542a63559bb336429ba4e3c490dfeba4714b9"
+  version "1.0.4"
+  sha256 "5ccfc4341c5b14477bc465a512a27a42a3f15497ff39fc4c1a37a8b50162d629"
 
   url "https://github.com/uidops/ganjoor/releases/download/#{version}/Ganjoor-#{version}-macOS-Tahoe.dmg"
   name "Ganjoor"
